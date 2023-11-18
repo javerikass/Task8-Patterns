97385d9f740e401742954e26bba43dad11bcd935
javerikass <javerikass@gmail.com>
2023-11-18 14:55:01 +0300

    finishing commit

--- src/main/java/ru/clevertec/bank/service/impl/UserServiceImpl.java
+++ src/main/java/ru/clevertec/bank/service/impl/UserServiceImpl.java
@@ -24,6 +24,13 @@ public class UserServiceImpl implements UserService {
         this.validator = validator;
     }
 
+    /**
+     * Создает нового пользователя на основе данных из объекта UserDto.
+     *
+     * @param userDto данные нового пользователя
+     * @return id созданного пользователя, завернутый в Optional, или пустое значение Optional, если
+     * данные пользователя некорректны.
+     */
     @Override
     public Optional<UUID> createUser(UserDto userDto) {
         try {
@@ -38,12 +45,24 @@ public class UserServiceImpl implements UserService {
         return result;
     }
 
+    /**
+     * Возвращает объект пользователя с указанным идентификатором.
+     *
+     * @param id идентификатор пользователя
+     * @return объект UserDto для пользователя с указанным идентификатором
+     * @throws NoSuchElementException если пользователя с указанным идентификатором не найден
+     */
     @Override
     public UserDto getUserById(UUID id) {
         return mapper.toUserDto(userDao.getUserById(id)
             .orElseThrow(() -> new NoSuchElementException("No user with id " + id)));
     }
 
+    /**
+     * Обновляет данные пользователя на основе объекта UserDto.
+     *
+     * @param updatedUser обновленные данные пользователя
+     */
     @Override
     public void updateUser(UserDto updatedUser) {
         try {
@@ -55,6 +74,11 @@ public class UserServiceImpl implements UserService {
         userDao.updateUser(mapper.toUser(updatedUser));
     }
 
+    /**
+     * Удаляет пользователя с указанным идентификатором.
+     *
+     * @param id идентификатор пользователя
+     */
     @Override
     public void deleteUser(UUID id) {
         userDao.deleteUser(id);
--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,10 +1,10 @@
-CREATE SCHEMA clevertec_system;
+CREATE SCHEMA IF NOT EXISTS clevertec_system;
 
-SET search_path TO  clevertec_system;
+SET search_path TO clevertec_system;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE users
+CREATE TABLE IF NOT EXISTS users
 (
     id         UUID DEFAULT uuid_generate_v4(),
     first_name VARCHAR(255),
@@ -15,9 +15,7 @@ CREATE TABLE users
 
 );
 
-
 INSERT INTO users (id, first_name, last_name, mail, age)
-VALUES ('11111111-1111-1111-1111-111111111111', 'John', 'Doe', 'john.doe@mail.ru', 25),
-       ('22222222-2222-2222-2222-222222222222', 'Jane', 'Smith', 'jane.smith@gmail.com', 30),
-       ('33333333-3333-3333-3333-333333333333', 'Alice', 'Johnson', 'alice.johnson@gmail.com',
-        28);
+VALUES ('37bc2b04-8607-11ee-b9d1-0242ac120002', 'John', 'Doe', 'john.doe@mail.ru', 40),
+       ('3ef4c1f6-8607-11ee-b9d1-0242ac120002', 'Jane', 'Smith', 'jane.smith@gmail.com', 30),
+       ('43c4ec92-8607-11ee-b9d1-0242ac120002', 'Alice', 'Johnson', 'alice.johnson@gmail.com', 28);
